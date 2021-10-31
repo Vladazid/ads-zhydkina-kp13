a80a99a84038766de5cd1712028b271971e46bfc
Vladazid <92862263+Vladazid@users.noreply.github.com>
2021-10-31 17:39:59 +0200

    Add files via upload

---
+++ лабораторна 1/Завдання 1/Task_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace _1task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, a, b;
+            Write("x = "); x = double.Parse(ReadLine());
+            Write("y = "); y = double.Parse(ReadLine());
+            Write("z = "); z = double.Parse(ReadLine());
+
+            if (x == 0 || y == 0 || z == 0)
+            {
+                WriteLine("помилка");
+            }
+            else
+            {
+                a = Cos(x + (x * y / z));
+                b = Pow(x, 3) / Cos(a);
+
+                WriteLine("a = " + a);
+                WriteLine("b = " + b);
+            }
+            ReadKey();
+        }
+    }
+}
---
+++ лабораторна 1/Завдання 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n;
+            bool Mersen_flag;
+            long i, j;
+            n = 1000000;
+
+            for (i = 2; i < n; i++)  // перевірка чисел 
+            {
+                bool flag_Prime = true;
+                for (j = 2; j <= Sqrt(i); j++)
+                    if (i % j == 0)
+                    {
+                        flag_Prime = !flag_Prime;
+                        break;
+                    }
+                if (flag_Prime)
+                {
+                    double p_log = Log(i + 1, 2);
+                    bool long_Log = Abs(Round(p_log) - p_log) < 1.0E-8;
+                    if (long_Log)
+                    {
+                        long P = (long)Round(p_log);
+                        Mersen_flag = true;
+                        for (j = 2; j <= Sqrt(P); j++)
+                            if (P % j == 0)
+                            {
+                                Mersen_flag = !Mersen_flag;
+                                break;
+                            }
+                        if (Mersen_flag)
+                            WriteLine("2^" + P.ToString() + "-1 = " + i.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
